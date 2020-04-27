cccd054fc04654db20f26b73519be1b917b8ac80
rasyidch <rasyidchamiedy@gmail.com>
2020-04-27 23:52:57 +0800

    tugas for modif

---
+++ src/tugas/t_For.java
@@ -0,0 +1,18 @@
+package tugas;
+
+public class t_For {public static void main(String[] args){
+    int isiBensin = 0;
+    int penuh = 50;
+
+    System.out.println("Isi Bahan Bakar Sekarang :" + isiBensin + "ltr");
+    System.out.println("Isi Bahan Bakar Sekarang :" + isiBensin + "ltr");
+
+    for (isiBensin = 0; isiBensin <= penuh ; isiBensin++){
+        System.out.println("sedang mengisi Bahan Bakar");
+        System.out.println("isi Bahan Bakar sekarang :"+isiBensin+"ltr");
+    }
+    System.out.println("Finale : isi Bahan Bakar sekarang :"+ isiBensin);
+
+
+}
+}
