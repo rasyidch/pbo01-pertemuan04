ff75e992984ee74c5a819dfbeed80ab8b6ca6e54
rasyidch <rasyidchamiedy@gmail.com>
2020-04-27 23:24:23 +0800

    array

---
+++ src/percabangan/array.java
@@ -0,0 +1,13 @@
+package percabangan;
+
+public class array {public static void main(String[] args) {
+    String matakuliah[];
+    matakuliah = new String[10];
+
+    matakuliah[0] = "Pemrograman Berbasis Objek 1";
+    matakuliah[1] = "visual 2";
+    matakuliah[2] = "rekayasa perangkat lunak";
+    matakuliah[3] = "organisasi komputer";
+    matakuliah[4] = "Pemrograman web 2";
+}
+}
