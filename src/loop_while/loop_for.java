37d8883c1863e57de07ab8b34bf89f1cce49affb
rasyidch <rasyidchamiedy@gmail.com>
2020-04-27 22:42:50 +0800

    loop for

---
+++ src/loop_while/loop_for.java
@@ -0,0 +1,20 @@
+package loop_while;
+
+public class loop_for {
+
+        public static void main(String[] args){
+            int isiGelas = 0;
+            int penuh = 225;
+
+            System.out.println("Isi Gelas Sekarang :" + isiGelas + "ml");
+            System.out.println("Isi Gelas Sekarang :" + isiGelas + "ml");
+
+            for (isiGelas = 0; isiGelas <= penuh ; isiGelas++){
+                System.out.println("sedang mengisi gelas");
+                System.out.println("isi gelas sekarang :"+isiGelas+"ml");
+            }
+            System.out.println("Finale : isi gelas sekarang :"+ isiGelas);
+
+
+    }
+}
